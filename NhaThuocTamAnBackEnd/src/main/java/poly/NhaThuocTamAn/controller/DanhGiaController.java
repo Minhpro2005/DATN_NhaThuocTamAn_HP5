6e554576a957f0d9699618825b42b22ce52b1086
quangminh529 <quangminhhp529@gmail.com>
2025-07-30 20:17:39 +0700

    ...

--- NhaThuocTamAnBackEnd/src/main/java/poly/NhaThuocTamAn/controller/DanhGiaController.java
+++ NhaThuocTamAnBackEnd/src/main/java/poly/NhaThuocTamAn/controller/DanhGiaController.java
@@ -1,12 +1,17 @@
 package poly.NhaThuocTamAn.controller;
 
+import java.io.IOException;
+import java.nio.file.*;
 import java.util.List;
+import java.util.UUID;
+
+import com.example.demo.dto.DanhGiaDTO;
+import poly.NhaThuocTamAn.service.DanhGiaService;
+import poly.NhaThuocTamAn.repository.DanhGiaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import poly.NhaThuocTamAn.model.DanhGia;
-import poly.NhaThuocTamAn.service.DanhGiaService;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/danh-gia")
@@ -14,21 +19,36 @@ public class DanhGiaController {
 	@Autowired
 	private DanhGiaService dgsrv;
 	
+	@Autowired
+	private DanhGiaRepository dgrep;
+	
 	//Get all
 	@GetMapping
-	public List<DanhGia> getAll(){
-		return dgsrv.getAll();
+	public List<DanhGiaDTO> getAll(){
+		return dgsrv.getAllDanhGia();
 	}
 	
-	//Get by ID
-	@GetMapping("/{maDG}")
-	public DanhGia getById(@PathVariable Integer maDG) {
-		return dgsrv.getById(maDG);
-	}
+	//Get by variation
+	@GetMapping("/bien-the/{maBienThe}")
+    public List<DanhGiaDTO> getByBienThe(@PathVariable Integer maBienThe) {
+        return dgsrv.getDanhGiaByBienThe(maBienThe);
+    }
+	
+	//Get by product
+    @GetMapping("/thuoc/{maThuoc}")
+    public List<DanhGiaDTO> getByThuoc(@PathVariable Integer maThuoc) {
+        return dgsrv.getDanhGiaByThuoc(maThuoc);
+    }
+    
+    //Post
+    @PostMapping
+    public DanhGiaDTO add(@RequestBody DanhGiaDTO dto) {
+        return dgsrv.addDanhGia(dto);
+    }
 		
-	//Delete
-	@DeleteMapping("/{maDG}")
-	public void delete(@PathVariable Integer maDG) {
-		dgsrv.deleteById(maDG);
-	}
+//	//Delete
+//	@DeleteMapping("/{maDG}")
+//	public void delete(@PathVariable Integer maDG) {
+//		dgsrv.deleteById(maDG);
+//	}
 }
